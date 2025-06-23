68a67d49bf686511564ffff992e6b0cd3f680c69
Tristan <129543913+tblackledge97@users.noreply.github.com>
2025-06-23 10:52:47 +0100

    Add files via upload
    twoSum github problem

---
+++ twoSum.py
@@ -0,0 +1,14 @@
+def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+
+        # outer loop
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
